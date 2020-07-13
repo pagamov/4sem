78474d4c2f03f1076201021a8c0169721003f271
Pavel Gamov <pagamov@gmail.com>
2020-07-13 17:00:55 +0300

    5f0849918169d748af865467

--- summer task/src/text_search/search_routes.js
+++ summer task/src/text_search/search_routes.js
@@ -32,42 +32,65 @@ module.exports = function (app, db) {
         }
     });
 
+    function ov(ts, db) {
+        db.collection('text').find( { t: { $in: ts } } , { _id: 0, t: 0, d: 1 }, (err, item) => {
+            return new Promise((resolve, reject) => {
+                if (err) {
+                    reject({ 'error': 'An error has occured' });
+                } else {
+                    console.log('item')
+                    resolve(item.toArray());
+                }
+            });
+        });
+    };
+
     app.get('/search', (req, res, next) => {
         // by not all text
         if (!req.body.ts) {
             next();
         } else {
             var ts = req.body.ts.split(' ');
-            // console.log(ts);
-            var result = new Map();
-
-            ts.forEach((title, i) => {
-                db.collection('text').findOne( {t: title}, { _id: 0, t: 0, d: 1 }, (err, item) => {
-                    if (err) {
-                        res.send({ 'error': 'An error has occured' });
-                    } else {
-                        // console.log(title + ' 2nd midd ware ' + item.d);
-                        // console.log(item);
-                        if (item) {
-                            console.log(item);
-                            // console.log(req.body.p);
-                            // console.log(item.d);
-                            console.log(Overlap(req.body.p, item.d));
-                            result.set(title, Overlap(req.body.p, item.d));
+            db.collection('text').find( { t: { $in: ts } } , { _id: 0, t: 0, d: 1 } ).toArray((err, item) => {
+                if (err) {
+                    res.send({ 'error': 'An error has occured' });
+                } else {
+                    async function f (req, item) {
+                        var result = [];
+                        for (const ite of item) {
+                            await result.push([ite._id, ite.t, Overlap(req.body.p, ite.d)]);
                         }
-                    }
-                });
-            });
+                        return result;
+                    };
+                    async function o (req, res, item) {
+                        var result = await f (req, item);
+                        res.send(result);
+                    };
 
-            console.log('res ' + result);
-            res.send({r: result});
-            // res.send('done 2');
+                    o(req, res, item);
+                }
+            });
         }
     });
 
     app.get('/search', (req, res) => {
-        // by all text
-        console.log('2nd midd ware');
-        res.send('done 2');
+        db.collection('text').find().toArray((err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+            } else {
+                async function f (req, item) {
+                    var result = [];
+                    for (const ite of item) {
+                        await result.push([ite._id, ite.t, Overlap(req.body.p, ite.d)]);
+                    }
+                    return result;
+                };
+                async function o (req, res, item) {
+                    var result = await f (req, item);
+                    res.send(result);
+                };
+                o(req, res, item);
+            }
+        });
     });
 };

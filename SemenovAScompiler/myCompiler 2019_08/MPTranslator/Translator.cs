d86ddedb6367cf9524f38c71789e0023699c9c5f
Pavel <52429594+pagamov@users.noreply.github.com>
2021-04-17 15:51:53 +0300

    sp done

--- SemenovAScompiler/myCompiler 2019_08/MPTranslator/Translator.cs
+++ SemenovAScompiler/myCompiler 2019_08/MPTranslator/Translator.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 
 namespace MPTranslator
 {
-
-    class Translator : myMp  
+    class Translator : myMp
     {
         //инициализация магазинных и операционных символов
-        public Translator(ArrayList Q, ArrayList Sigma, ArrayList Gamma, string Q0, string z0, ArrayList F) : 
+        public Translator(ArrayList Q, ArrayList Sigma, ArrayList Gamma, string Q0, string z0, ArrayList F) :
             base(Q,Sigma, Gamma,Q0,z0, F) { }
 
         public override bool Execute(string str)
@@ -22,21 +21,16 @@ namespace MPTranslator
             ArrayList list = new ArrayList();
             int i = 0;
             int j = 0;
-            str = str + "e"; //empty step вставить "" не получается, так как это считается пустым символом, 
+            str = str + "e"; //empty step вставить "" не получается, так как это считается пустым символом,
                              //который не отображается в строке
             string s;
             //delta.debug();
-            for (; ; )
-            {
-                if (delta == null)
-                {
+            for ( ; ; ) {
+                if (delta == null) {
                     return false;
                 }
-                
-                if (delta.LeftT != "")  // И В ВЕРШИНЕ СТЕКА ТЕРМИНАЛЬНЫЙ СИМВОЛ LeftT!!!! пустой такт
-                {
-                    for (; i < str.Length;) //модель считывающего устройства
-                    {
+                if (delta.LeftT != "") { // И В ВЕРШИНЕ СТЕКА ТЕРМИНАЛЬНЫЙ СИМВОЛ LeftT!!!! пустой такт
+                    for (; i < str.Length;) { //модель считывающего устройства
                         if (Z.Peek().ToString() == str[i].ToString())
                         {
                             this.Z.Pop();
@@ -45,25 +39,22 @@ namespace MPTranslator
                         } else return false;
                         break;
                     }
-                } else if (delta.LeftT == "")  // И В ВЕРШИНЕ СТЕКА НЕ ТЕРМИНАЛЬНЫЙ СИМВОЛ LeftT!!!!
-                {
-                    //шаг 1 вытолкнуть из стека и занести в стек rightZ 
+                } else if (delta.LeftT == "") { // И В ВЕРШИНЕ СТЕКА НЕ ТЕРМИНАЛЬНЫЙ СИМВОЛ LeftT!!!!
+                    //шаг 1 вытолкнуть из стека и занести в стек rightZ
                     this.Z.Pop();
                     s = arrToStr(delta.RightZ);
                     for (j = s.Length - 1; j >= 0; j--) this.Z.Push(s[j]);
                 }
-                
                 Infix_convert((DeltaQSigmaGammaSix)delta, list); // перевод в обратную польскую
-                
-                if (this.Z.Count != 0)
-                {
+                if (this.Z.Count != 0) {
                     currState = arrToStr(delta.RightQ);
                     delta = findDelta(currState, Z.Peek().ToString());
                     //delta.debug();
-                } else if (str[i].ToString() == "e")
-                {
+                } else if (str[i].ToString() == "e") {
                     return true;
-                } else return false;
+                } else {
+                    return false;
+                }
                 Console.WriteLine();
             } // end for
             //проверка на терминал или нетерминал в вершине стека
@@ -72,40 +63,29 @@ namespace MPTranslator
 
         public void Infix_convert (DeltaQSigmaGammaSix delta, ArrayList list)
         {
-      //  list.Add(delta.rightSix.Clone()); 
-      if (list.Count == 0)
-            {    //Для самого первого дельта-правила заносим правило перевода (3 элемента)
-             foreach (string d in delta.rightSix)
-                {
+      //  list.Add(delta.rightSix.Clone());
+            if (list.Count == 0) {   //Для самого первого дельта-правила заносим правило перевода (3 элемента)
+                foreach (string d in delta.rightSix) {
                     list.Add(d);
-
                     Console.Write(d);
                     Console.Write(" ");
                 }
                 Console.WriteLine();
-            } else //Если уже есть правило, то делаем в нем замены
-            {
+            } else {//Если уже есть правило, то делаем в нем замены
                 //Ищем символ в котором происходит изменение (например P->i)
-                for (int i = 0; i < list.Count; i++)
-                {
-
+                for (int i = 0; i < list.Count; i++) {
                     //если символ найдем меняем его и завершаем обход
                     if (delta.LeftZ.Equals(list[i].ToString())) {
                         list[i] = Utility.convert(delta.rightSix);
                         break;
                     }
                 }
-                //Печатаем выходную строку
-                for (int i = 0; i < list.Count; i++)
-                {
+                for (int i = 0; i < list.Count; i++) { //Печатаем выходную строку
                     Console.Write(list[i]);
                     Console.Write(" ");
                 }
                 Console.WriteLine();
-
             }
         }
-
-    } // end class
-
+    }
 }
